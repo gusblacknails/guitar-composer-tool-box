57f4cffff1177fa305c7d9eceef84be82e6c865c
Gus Escola <camagrock@gmail.com>
2019-10-21 17:34:18 +0100

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
 import Home from "../components/Home"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 import GuitarChordFinder from "./guitar-chord-finder"
 const IndexPage = () => (
